f05174cefc679a72945e0844987c85075c8d929e
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-24 14:59:32 +0530

    update

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <div className="bg"></div>
+      <h1>Day-1 React.js</h1>
+      <h2>hello</h2>
     </div>
   );
 }
