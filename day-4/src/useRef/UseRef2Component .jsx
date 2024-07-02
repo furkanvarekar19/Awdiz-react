eb80fa5419655643d1719a555e661eda8f329ba4
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-07-02 12:14:01 +0530

    update

--- day-4/src/useRef/UseRef2Component .jsx
+++ day-4/src/useRef/UseRef2Component .jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Usestate from '../usestate/Usestate';
 
 function UseRef2Component () {
+
+  const counterRef = useRef(0);
+  const [counter , setCounter] = useState(10);
+   
+  function Increment(){
+    counterRef.current++;
+    console.log(counterRef.current , " current counterRef");
+  }
+
   return (
-    <div>
-        <h1>UseRef-2 Component</h1>
+    <div style={{textAlign:'center'}}>
+        <h1>UseRef-2 Component</h1> <br />
+          <h1>counterRef : {counterRef.current} </h1>
+          <h2> State counter : {counter} </h2> <br />
+          <button onClick={()=> setCounter(counter + 1)}>+Increment State</button> <br /> <br />
+          <button onClick={Increment}>+Increment Ref</button>
     </div>
   )
 }
--- day-4/src/useRef/UseRefComponent .jsx
+++ day-4/src/useRef/UseRefComponent .jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function UseRefComponent () {
+  const [counter , setCounter] = useState(0);
+  console.log(counter , "counter");
+  const prevCounter = useRef();
+  console.log(prevCounter , "prevCounter");
+
+  useEffect(()=>{
+  prevCounter.current = counter;
+  },[counter])
+
+
   return (
-    <div>
-        <h1>UseRefComponent</h1>
+    <div style={{textAlign:'center'}}>
+        <h1>UseRefComponent</h1> <br />
+        <h2>prevCounter : {prevCounter.current} </h2><br />
+        <h2>Counter : {counter}</h2>
+        <button onClick={()=> setCounter(counter + 1)}>+</button>
     </div>
   )
 }
